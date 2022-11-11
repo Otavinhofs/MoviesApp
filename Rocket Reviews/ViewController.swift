d3176834f08aa1f820143835afd859444cfe4717
Julia Eileen Schäfer <julia.schafer@vortigo.com.br>
2022-11-11 10:46:40 -0300

    Initial Commit

---
+++ Rocket Reviews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rocket Reviews
+//
+//  Created by Julia Eileen Schäfer on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
